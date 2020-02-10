1e7094b61ef25420bc495a22178459f468af9fc6
persocom01 <persocom02@gmail.com>
2020-02-10 14:12:21 +0800

    0.22

--- layout/scripts/backtotop.js
+++ layout/scripts/backtotop.js
@@ -1,15 +1,16 @@
 // Displays and hides the scroll to top button depending on scroll position.
-window.onscroll = function () { scrollFunction() }
+scrollPos = 100
+window.onscroll = function () { returntoTop() }
 
-function scrollFunction () {
+function returntoTop () {
   // if scroll > 20px.
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > scrollPos || document.documentElement.scrollTop > scrollPos) {
     // The scale method of making a button appear. The display method would be:
-    // document.getElementById('but2').style.display = 'inline-block'
-    document.getElementById('but2').style.transform = 'scale(1)'
+    // document.getElementById('backtotop').style.display = 'inline-block'
+    document.getElementById('backtotop').style.transform = 'scale(1)'
   } else {
     // The scale method of making a button appear. The display method would be:
-    // document.getElementById('but2').style.display = 'none'
-    document.getElementById('but2').style.transform = 'scale(0)'
+    // document.getElementById('backtotop').style.display = 'none'
+    document.getElementById('backtotop').style.transform = 'scale(0)'
   }
 }
