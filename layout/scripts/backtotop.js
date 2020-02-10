8749822b2bda958c1c81c80ee7d673bef7e77694
persocom01 <persocom02@gmail.com>
2020-02-10 14:14:33 +0800

    0.22

--- layout/scripts/backtotop.js
+++ layout/scripts/backtotop.js
@@ -1,8 +1,8 @@
 // Displays and hides the scroll to top button depending on scroll position.
-scrollPos = 100
-window.onscroll = function () { returntoTop() }
+var scrollPos = 100
+window.onscroll = function () { btnAppear() }
 
-function returntoTop () {
+function btnAppear () {
   // if scroll > 20px.
   if (document.body.scrollTop > scrollPos || document.documentElement.scrollTop > scrollPos) {
     // The scale method of making a button appear. The display method would be:
